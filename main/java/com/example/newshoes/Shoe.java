47948c7ad52fb06a9eb4275d2774d59aa68c79c5
Jesse Blackledge <jesse.m.blackledge-1@ou.edu>
2019-12-20 05:15:39 -0600

    Implemented the Serializable interface to allow a Shoe object to be passed to an activity with an Intent

--- main/java/com/example/newshoes/Shoe.java
+++ main/java/com/example/newshoes/Shoe.java
@@ -1,8 +1,9 @@
 package com.example.newshoes;
 
+import java.io.Serializable;
 import java.util.ArrayList;
 
-public class Shoe {
+public class Shoe implements Serializable {
     private String shoeName;
 
     private Double desiredDistanceInMiles;
