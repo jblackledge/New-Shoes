d49d9d275773f608c8d3cfa1d25e0f4a674dbe6e
Jesse Blackledge <jesse.m.blackledge-1@ou.edu>
2019-12-20 05:18:17 -0600

    Added to onCreate() method to display the passed in Shoe toString on the Activity screen as a TextView

--- main/java/com/example/newshoes/StartRun.java
+++ main/java/com/example/newshoes/StartRun.java
@@ -2,7 +2,9 @@ package com.example.newshoes;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class StartRun extends AppCompatActivity {
 
@@ -10,5 +12,11 @@ public class StartRun extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start_run);
+
+        Intent intent = getIntent();
+        Shoe shoe = (Shoe)intent.getSerializableExtra("Shoe");
+
+        TextView testingShoe = findViewById(R.id.test_passed_shoe);
+        testingShoe.setText(shoe.toString());
     }
 }
