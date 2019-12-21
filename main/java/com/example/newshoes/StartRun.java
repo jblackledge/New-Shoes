fe44c1c7a69dc750f09658137b5260aad0c7c504
Jesse Blackledge <jesse.m.blackledge-1@ou.edu>
2019-12-21 15:10:06 -0600

    Began implementing mile tracker using Location class

--- main/java/com/example/newshoes/StartRun.java
+++ main/java/com/example/newshoes/StartRun.java
@@ -2,8 +2,13 @@ package com.example.newshoes;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.widget.Switch;
 import android.widget.TextView;
 
 public class StartRun extends AppCompatActivity {
@@ -18,5 +23,26 @@ public class StartRun extends AppCompatActivity {
 
         TextView testingShoe = findViewById(R.id.test_passed_shoe);
         testingShoe.setText(shoe.toString());
+    }
+
+    public void trackRun() {
+        Switch trackRunSwitch = findViewById(R.id.track_run_switch);
+
+        Criteria criteria = new Criteria();
+        criteria.setAccuracy(Criteria.ACCURACY_FINE);
+
+        LocationManager locationManager =
+                (LocationManager)getSystemService(Context.LOCATION_SERVICE);
+        String providerName = locationManager.getBestProvider(criteria, true);
+
+        Location startLocation = new Location(providerName);
+        Double latitude = startLocation.getLatitude();
+        startLocation.setLatitude(latitude);
+
+        Double longitude = startLocation.getLongitude();
+        startLocation.setLongitude(longitude);
+        startLocation.set(startLocation);
+
+
     }
 }
