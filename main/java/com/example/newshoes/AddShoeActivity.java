3812c89e02ec780e034b802b602f1b1621234c1d
Jesse Blackledge <jesse.m.blackledge-1@ou.edu>
2019-12-30 04:48:40 -0600

    Added methods to save and load the ArrayList of Shoes

--- main/java/com/example/newshoes/AddShoeActivity.java
+++ main/java/com/example/newshoes/AddShoeActivity.java
@@ -3,14 +3,21 @@ package com.example.newshoes;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.ListActivity;
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
 
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import java.lang.reflect.Type;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
+import java.util.List;
 
 import static java.util.Collections.*;
 
@@ -41,6 +48,8 @@ public class AddShoeActivity extends AppCompatActivity {
         shoeList.add(myShoe);
         Collections.sort(shoeList);
 
+        saveSharedPreferencesShoeList(this, shoeList);
+
         Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
     }
@@ -58,6 +67,7 @@ public class AddShoeActivity extends AppCompatActivity {
 //    }
 
     public static ArrayList<Shoe> getShoeList() {
+
         return shoeList;
     }
 
@@ -68,4 +78,35 @@ public class AddShoeActivity extends AppCompatActivity {
 //    public Integer getDistance() {
 //        return myShoe.getDesiredDistanceInMiles();
 //    }
+
+    public static void saveSharedPreferencesShoeList(Context context, ArrayList<Shoe> shoeList) {
+        SharedPreferences sharedPreferences =
+                context.getSharedPreferences("ShoeList", context.MODE_PRIVATE);
+
+        SharedPreferences.Editor preferencesEditor = sharedPreferences.edit();
+        Gson gson = new Gson();
+        String json = gson.toJson(shoeList);
+        preferencesEditor.putString("shoe", json);
+        preferencesEditor.commit();
+    }
+
+    public static ArrayList<Shoe> loadSharedPreferencesShoeList(Context context) {
+        shoeList = new ArrayList<>();
+        SharedPreferences sharedPreferences =
+                context.getSharedPreferences("ShoeList", Context.MODE_PRIVATE);
+
+        Gson gson = new Gson();
+        String json = sharedPreferences.getString("shoe", "");
+
+        if(json.isEmpty())
+        {
+            shoeList = new ArrayList<>();
+        }
+        else
+        {
+            Type type = new TypeToken<ArrayList<Shoe>>() {}.getType();
+            shoeList = gson.fromJson(json, type);
+        }
+        return shoeList;
+    }
 }
