f68bbc9d94c025f8ceee0c762de752333dbb3b9d
Jesse Blackledge <jesse.m.blackledge-1@ou.edu>
2019-12-31 08:46:13 -0600

    Created empty method stub saveChanges() for the Save Changes Button onClick action

--- main/java/com/example/newshoes/EditShoe.java
+++ main/java/com/example/newshoes/EditShoe.java
@@ -3,6 +3,7 @@ package com.example.newshoes;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class EditShoe extends AppCompatActivity {
 
@@ -10,5 +11,9 @@ public class EditShoe extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_shoe);
+    }
+
+    public void saveChanges(View view) {
+
     }
 }
