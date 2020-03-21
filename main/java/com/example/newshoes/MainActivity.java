04fa57f0bb7aa996dc7c8fd8f5375cb52f0e664b
Jesse Blackledge <jesse.m.blackledge-1@ou.edu>
2020-03-20 21:04:29 -0500

    Added comments to the main() method

--- main/java/com/example/newshoes/MainActivity.java
+++ main/java/com/example/newshoes/MainActivity.java
@@ -24,15 +24,23 @@ public class MainActivity extends AppCompatActivity implements Serializable {
         showShoeList();
     }
 
+    /**
+     * When called, starts the AddShoeActivity
+     */
     public void newShoe(View view) {
         Intent intent = new Intent(this, AddShoeActivity.class);
         startActivity(intent);
     }
 
+    /**
+     * Alphabetically displays the list of shoes saved in shared preferences
+     */
     public void showShoeList() {
         Spinner spinner = findViewById(R.id.choose_shoe_spinner);
+        //loads the list of Shoe's from shared preferences and assigns it to an ArrayList of Shoes
         ArrayList<Shoe> shoeList = AddShoeActivity.loadSharedPreferencesShoeList(this);
 
+        //displays the list in the spinner
         ArrayAdapter<Shoe> spinnerArrayListAdapter = new ArrayAdapter<>(this,
                 android.R.layout.simple_spinner_item, shoeList);
 
@@ -42,43 +50,67 @@ public class MainActivity extends AppCompatActivity implements Serializable {
         spinner.setAdapter(spinnerArrayListAdapter);
     }
 
+    /**
+     * When called, moves to the StartRunActivity as long as there is a Shoe object in the spinner
+     */
     public void startRun(View view) {
         Spinner spinner = findViewById(R.id.choose_shoe_spinner);
         Shoe chosenShoe = (Shoe)spinner.getSelectedItem();
 
         Intent intent = new Intent(this, StartRun.class);
+        //passes the value of the Shoe object chosen from the Spinner list to the next activity
         intent.putExtra("Shoe", chosenShoe);
         if(chosenShoe != null) {
+            //only start the activity if the shoe object isn't null, this check prevents us from
+            //throwing a NullPointerException
             startActivity(intent);
         }
+        //If the Shoe object is null, we inform the user via a toast, to create a Shoe to track
+        //before trying to move to the StartRunActivity
         else{
             Toast toast = Toast.makeText(this, "Create a shoe before starting run", Toast.LENGTH_LONG);
             toast.show();
         }
     }
 
+    /**
+     * When called, deletes the Shoe object chosen in the spinner, if there are any Shoe objects
+     * in the Spinner
+     */
     public void deleteShoe(View view) {
         Spinner spinner = findViewById(R.id.choose_shoe_spinner);
         Shoe chosenShoe = (Shoe)spinner.getSelectedItem();
+        //Here we perform a null check to prevent a NullPointerException when attempting to delete
+        //a Shoe object. If null, we inform the user that there is no Shoe object in the list to
+        //delete
         if(chosenShoe == null)
         {
+            //informs the user, via a toast, that there is no shoe object in the list to delete
             Toast toast = Toast.makeText(this, "Nothing to delete", Toast.LENGTH_SHORT);
             toast.show();
         }
+        //If there is a shoe, we delete it, update our shared preferences list, and then update the
+        //spinner
         AddShoeActivity.deleteShoe(chosenShoe);
         AddShoeActivity.saveSharedPreferencesShoeList(this, AddShoeActivity.getShoeList());
         showShoeList();
     }
 
+    /**
+     * When called, if there are Shoe's in the spinner, loads the EditShoeActivity
+     */
     public void editShoe(View view) {
         Spinner spinner = findViewById(R.id.choose_shoe_spinner);
         Shoe chosenShoe = (Shoe)spinner.getSelectedItem();
         Intent intent = new Intent(this, EditShoe.class);
+        //passes the value of the Shoe object chosen from the Spinner list to the next activity
         intent.putExtra("Shoe", chosenShoe);
+        //null check to prevent NullPointerException
         if(chosenShoe != null)
         {
             startActivity(intent);
         }
+        //informs user, via a toas, that there is no Shoe object in the list to edit
         else
         {
             Toast toast = Toast.makeText(this, "Nothing to edit", Toast.LENGTH_SHORT);
