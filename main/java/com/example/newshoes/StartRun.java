c4c01c2f505825bffeb0ee4a05622b938ca4e843
Jesse Blackledge <jesse.m.blackledge-1@ou.edu>
2020-03-21 00:04:04 -0500

    Added comments to variables and methods

--- main/java/com/example/newshoes/StartRun.java
+++ main/java/com/example/newshoes/StartRun.java
@@ -26,28 +26,44 @@ import java.util.ArrayList;
 
 public class StartRun extends AppCompatActivity implements LocationListener {
 
-//    private final double LOCATION_CHANGED_LIMITATION = .00725;
+    //this is a constant value that is used to set a limit on when we can update the location
+    //if the distance between the new location and the current location is greater than this value
+    //we increment the mile counter, and update the location. If it's less than this value, we do
+    //nothing and keep listening. This allows us to prevent the mile count from "floating" and
+    //incrementing upon a false GPS change when the location updates
     private final double LOCATION_CHANGED_LIMITATION = .009;
 
     private final Integer METERS_IN_A_MILE = 1609;
 
+    //the original location that we start at. This location is updated each time we increment the
+    //mile count
     private Location startLocation;
 
+    //the location that we get when we get new input from the location listener. This variable
+    //allows us to find the distance between currentLocation and startLocation to update the mile
+    //count
     private Location currentLocation;
 
+    //Number of miles traveled during the current run
     private Double totalMilesTraveled;
 
     private TextView startLocationText;
 
     private TextView currentLocationText;
 
+    //TextView variable that updates the number of miles ran, with the totalMilesTraveled value
     private TextView trackedMiles;
 
     private Shoe shoe;
 
+    //Used to keep track of whether a toast has been made. Honestly, bool would be more efficient in
+    //this case since we only intend to display the toast once per run
     private Integer toastTally;
 
+    //Green progress bar that allows the user to visually see how far they've traveled so far in
+    //the selected pair of shoes
     private ProgressBar progressBar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -57,22 +73,34 @@ public class StartRun extends AppCompatActivity implements LocationListener {
         toastTally = 0;
 
         Intent intent = getIntent();
+        //gets the Shoe object placed in the intent
         shoe = (Shoe) intent.getSerializableExtra("Shoe");
 
+        //displays the Shoe object on the top of the acitvity using its toString value
         TextView testingShoe = findViewById(R.id.test_passed_shoe);
         testingShoe.setText(shoe.toString());
 
         progressBar = findViewById(R.id.run_progress_bar);
 
+        //Sets the max value of the progress bar to that of the desired distance of the current Shoe
+        //object. Here we convert from miles to meters
         progressBar.setMax(shoe.getDesiredDistanceInMiles().intValue() * METERS_IN_A_MILE);     //Changed progress bar to meters instead of miles
+        //Here we import the progress from previous runs and set that as the value level of the
+        //progress bar
         progressBar.setProgress(shoe.getMeterCount().intValue());             //Changed progress bar to meters instead of miles
 
+        //This TextVies displays the desired distance in miles at the far right end of the progress
+        //bar
         TextView progressBarDistanceText = findViewById(R.id.progress_bar_end_text);
         progressBarDistanceText.setText(shoe.getDesiredDistanceInMiles().toString());
 
+        //checks if the user has already give the app permission to access location
         checkLocationPermission();
     }
 
+    /**
+     * Method to get the location we are beginning our run at
+     */
     public void getStartLocation(View view) {
         Button pauseRun = findViewById(R.id.pause_run_button);
         Button stopRun = findViewById(R.id.stop_run_button);
@@ -89,6 +117,7 @@ public class StartRun extends AppCompatActivity implements LocationListener {
 
         String provider = locationManager.getBestProvider(criteria, true);
 
+        //if we dont't have permission from the user, we're done here
         if (checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION)
                 != PackageManager.PERMISSION_GRANTED &&
                 checkSelfPermission(Manifest.permission.ACCESS_COARSE_LOCATION) !=
@@ -102,11 +131,16 @@ public class StartRun extends AppCompatActivity implements LocationListener {
             // for Activity#requestPermissions for more details.
             return;
         }
+        //assigns the start location with the current GPS location
         startLocation = locationManager.getLastKnownLocation(provider);
 
         trackRun(view);
     }
 
+    /**
+     * Method that sets the currentLocation, and creates a lcationListener to check if the location
+     * has changes
+     */
     public void trackRun(View view) {
         totalMilesTraveled = 0.0;
         trackedMiles = findViewById(R.id.mile_count_text);
@@ -117,6 +151,7 @@ public class StartRun extends AppCompatActivity implements LocationListener {
         LocationManager locationManager =
                 (LocationManager) getSystemService(Context.LOCATION_SERVICE);
 
+        //if we dont't have permission from the user, we're done here
         if (checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION)
                 != PackageManager.PERMISSION_GRANTED &&
                 checkSelfPermission(Manifest.permission.ACCESS_COARSE_LOCATION) !=
@@ -138,14 +173,22 @@ public class StartRun extends AppCompatActivity implements LocationListener {
 
         trackedMiles.setText(String.format("%.2f", totalMilesTraveled));
 
+        //creates a location listener to update onLocationChanged method upon meeting the specified
+        //time and/or distance criteria
         locationManager.requestLocationUpdates(provider, 100, 2, this);
     }
 
+    //Method to add miles to the current Shoe object whenever the End Run button is pressed
     public void addMilesToShoe(View view) {
         ArrayList<Shoe> myList = AddShoeActivity.getShoeList();
 
+        //Since Java is pass by value, we have to loop through the ArrayList of Shoes and find
+        //the shoe with the same toString value as the Shoe that was passed in to the activity
         for(Shoe shoe : myList)
         {
+            //Once we find the shoe, we remove it, update the mile count of the Shoe from this
+            //activity, and then insert it into the place of the shoe we just removed. Once this is
+            //accomplished, we break out of the loop to avoid errors
             if(this.shoe.toString().equals(shoe.toString()))
             {
                 int indexOfShoeInList = myList.indexOf(shoe);
@@ -155,11 +198,17 @@ public class StartRun extends AppCompatActivity implements LocationListener {
                 break;      //without break, throws invokation target exception
             }
         }
+        //save the list of Shoes to shared preferences
         AddShoeActivity.saveSharedPreferencesShoeList(this, myList);
+        //go back to the main activity
         Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
     }
 
+    /**
+     * Method to check if we have permission from the user to access their location. If not we
+     * prompt them with a dialogue box and request permission from them
+     */
     public boolean checkLocationPermission() {
         final int MY_PERMISSIONS_REQUEST_LOCATION = 99;
 
@@ -237,6 +286,9 @@ public class StartRun extends AppCompatActivity implements LocationListener {
         }
     }
 
+    /**
+     * Method called when the locationListener sees that we've changed location
+     */
     @Override
     public void onLocationChanged(Location location) {
         final Double VALUE_OF_MILE_IN_METERS = 0.000621371;
@@ -251,6 +303,7 @@ public class StartRun extends AppCompatActivity implements LocationListener {
         String provider = locationManager.getBestProvider(criteria, true);
 
 
+        //if we dont't have permission from the user, we're done here
         if (checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION)
                 != PackageManager.PERMISSION_GRANTED &&
                 checkSelfPermission(Manifest.permission.ACCESS_COARSE_LOCATION) !=
@@ -262,15 +315,22 @@ public class StartRun extends AppCompatActivity implements LocationListener {
             //                                          int[] grantResults)
             // to handle the case where the user grants the permission. See the documentation
             // for Activity#requestPermissions for more details.
-            trackedMiles.setText("Lol nope");
             return;
         }
+        //update currentLocation
         currentLocation = locationManager.getLastKnownLocation(provider);
 
+        //calculate the meters between our currentLocation and our startLocation
         Float metersBetween = currentLocation.distanceTo(startLocation);
+        //convert Float value to a Double value
         Double metersBetweenDouble = metersBetween.doubleValue();
+        //convert meters to miles
         Double milesBetween = metersBetweenDouble * VALUE_OF_MILE_IN_METERS;
 
+        //if the milesBetween our current and start locations is greater than the value set by the
+        //LOCATION_CHANGED_LIMITATION constant, we continue on. Else we return and wait for another
+        //call to the method. This prevents us from falsely incrementing our totalMilesTraveled,
+        //when the GPS is "floating"
         if(milesBetween < LOCATION_CHANGED_LIMITATION)
         {
             return;
@@ -281,8 +341,16 @@ public class StartRun extends AppCompatActivity implements LocationListener {
         trackedMiles.setText(String.format("%.2f", totalMilesTraveled));
         shoe.setMeterCount(metersBetweenDouble);
         updateProgressBar(shoe.getMeterCount().intValue());        //Changed progress bar to meters instead of miles
+        //set the value of startLocation to currentLocation. This allows us to only count recent
+        //changes, otherwise, if startLocation stays the same as the beginning, we are adding the
+        //incorrect amount of miles, because we are always checking the distance between
+        //start and current location
         startLocation = currentLocation;
 
+        //this checks if we've reached our mile goal set by the user for this pair of Shoes. If we
+        //have, we display a Toast that alerts the user that it's time for a new pair of shoes.
+        //toastTally only allows us to make the Toast once
+        //I can implement this better though
         if( (totalMilesTraveled >= shoe.getDesiredDistanceInMiles()) && toastTally < 1)
         {
             Context context = getApplicationContext();
@@ -294,11 +362,20 @@ public class StartRun extends AppCompatActivity implements LocationListener {
         }
     }
 
+    /**
+     * Method to update the progress bar that is called after every successful call to the
+     * onLocationChanged method
+     */
     public void updateProgressBar(int progress) {
         progressBar = findViewById(R.id.run_progress_bar);
         progressBar.setProgress(progress);
     }
 
+    /**
+     * Method called when the Pause Run button is pressed. This method turns off the
+     * locationListener by calling its removeUpdates method, then alerts the user that the run has
+     * been paused, via a Toast
+     */
     public void pauseRun(View view) {
         LocationManager locationManager =
                 (LocationManager) getSystemService(Context.LOCATION_SERVICE);
